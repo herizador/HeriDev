b9af9b7db8ec14a2f4241cc844cda235046714fd
Ismael <id386827@gmail.com>
2026-06-24 22:12:59 +0200

    feat: añadir recurso de favicon oficial para la marca HeriDev

--- .vercel/output/functions/_render.func/dist/server/entry.mjs
+++ .vercel/output/functions/_render.func/dist/server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_C-yB4CfW.mjs';
-import { manifest } from './manifest_C-FSg2id.mjs';
+import { manifest } from './manifest_BYaFr9SF.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -23,7 +23,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "99dd9d08-dfb2-4ff0-a110-cc774a71e944",
+    "middlewareSecret": "b2a3065c-033c-4340-a5f8-b5f0ca4f038b",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
